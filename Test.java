e23f200e7b19a43c6be9bd78f991a0b0edbd9054
Junmo Park <jmjm9797@naver.com>
2022-01-01 21:10:46 +0900

    백준

---
+++ Test.java
@@ -0,0 +1,13 @@
+package green.kr.ca.Hello;
+
+public class Test{
+
+	   long Sum(int[] a) {
+	      long sum = 0;
+	      for(int i=0;i<a.length;i++) {
+	         sum += a[i];
+	      }
+	      return sum;
+	   }
+	   
+	}
