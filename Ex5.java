ecf21b0c5865dbd76675b9694e272754f6f739d0
Junmo Park <91249513+momoJune@users.noreply.github.com>
2022-03-19 22:23:00 +0900

    백준

--- Ex5.java
+++ Ex5.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Ex5 {
 	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
-//		
-//		String word = sc.next();					// 문자열 입력
-		for(int i = 97;i<=122;i++) {
-		System.out.println((char)i);
-		}
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		str += "??!";
+		System.out.println(str);
 	}
 }
