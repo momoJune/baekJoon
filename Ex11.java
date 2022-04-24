3e432f746b908f8976e84d25ba701118c795f4c2
Junmo Park <91249513+momoJune@users.noreply.github.com>
2022-04-24 21:17:42 +0900

    백준 수학문제 01

--- Ex11.java
+++ Ex11.java
@@ -1,16 +1,27 @@
-package green.kr.ca.Hello;
+package beak;
+
 import java.util.Scanner;
 
 public class Ex11 {
-	   public static void main(String[] args) {
-	      Scanner sc = new Scanner(System.in);
-	      int n = sc.nextInt();
-	      int x = sc.nextInt();
-	      for (int i = 1; i <= n; i++) {
-	         int a = sc.nextInt();
-	         if(a>=1 && a<=10000) {
-	            if(a<x)System.out.print(a +" ");
-	         }
-	      }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		long a = sc.nextInt(); // 1000
+		long b = sc.nextInt(); // 70
+		long c = sc.nextInt(); // 170
+		long count = 1;
+		if(b>=c) {
+			count=-1;
+			System.out.println(count);
+		}
+
+		if (c - b > 0) { // 순이익이 0이상
+			if (a % (c - b) > 0) { // 고정비를 순이익으로 나눈 나머지가 없으면 한대만 더팔면 되므로
+				count = (a / (c - b))+1;
+			} else if (a % (c - b) == 0) { // 고정비를 순이익으로 나눈 나머지가 0이상이면 그 몫이 답.
+				count = a / (c - b) + 1;
+			}
+			System.out.println(count);
+		}
 	}
 }
