d0309280376683863e7b564e6b65db9122390b1e
Junmo Park <91249513+momoJune@users.noreply.github.com>
2022-05-02 21:17:18 +0900

    Add files via upload
    백준문제

--- Ex17.java
+++ Ex17.java
@@ -1,17 +1,22 @@
-package green.kr.ca.Hello;
+package beak;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
+// 문제 : https://www.acmicpc.net/problem/1157
 public class Ex17 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] n = new int[10];
-		int namu;
-
-		for (int i = 0; i < 10; i++) {
-			n[i] = sc.nextInt();
-			namu = n[i] % 42;
-			System.out.println(namu);
+		
+		int countMax = 0;
+		String str = sc.next();
+		String[] strList = str.split("");
+		ArrayList<String> strArrayList = new ArrayList<String>(Arrays.asList(strList));
+		
+		for(int i = 0; i < strArrayList.size();i++) {
+			for(int j = 0; j < strArrayList.size();j++) {
+			if(strArrayList.get(i).equals(strArrayList.get(j)))
 		}
 	}
 }
