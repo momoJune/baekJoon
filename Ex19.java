3a6fdd187ab83aab9541497ba31cd69acfc0ac44
Junmo Park <91249513+momoJune@users.noreply.github.com>
2022-05-14 23:32:49 +0900

    백준

--- Ex19.java
+++ Ex19.java
@@ -1,37 +1,34 @@
-package green.kr.ca.Hello;
+package beak;
 
 import java.util.Scanner;
 
+// 문제 : https://www.acmicpc.net/problem/17478
 public class Ex19 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] c = new int[sc.nextInt()];
-		int[] score = null;
-		int sum = 0;
-		double avg = 0;
-		int count = 0;
-		double per = 0;
-
-		for (int i = 0; i < c.length; i++) {
-			int n = sc.nextInt();
-			if(!(n>=1 && n<=1000))break; 
-			score = new int[n];
-
-			for (int j = 0; j < n; j++) {
-				score[j] = sc.nextInt();
-				if (score[j] >= 0 && score[j] <= 100) sum += score[j];
-			}
-			avg = (double)(sum)/n;
-			
-			for(int k = 0; k < n;k++) {
-				if(score[k]>avg) {
-					count++;
-				}
-			}
-			per = (double)(count)/n*100;
-			System.out.printf("%.3f%%\n",per);
-			sum =0; count =0;
-			
+		
+		int N = sc.nextInt();
+		String str1 = "____"; 
+		String str2 = "____";
+		
+		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n" + 
+				"\"재귀함수가 뭔가요?\"\n" + 
+				"\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n" + 
+				"마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n" + 
+				"그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+		for(int i = 0; i<N-1;i++) {
+			System.out.println(str1 + "\"재귀함수가 뭔가요?\"\n" + str1+
+				"\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n" + str1 +
+				"마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n" +  str1 +
+				"그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+			str1 += str2;
+		}
+		System.out.println(str1 + "\"재귀함수가 뭔가요?\"\n" + str1 + 
+				"\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+		for(int j = 0; j<N+1; j++) {
+			System.out.println(str1 + "라고 답변하였지.");
+			if(str1.length()==0)break;
+			str1 = str1.substring(4);
 		}
 	}
 }
